9a60e0dfeb5032955d7501ee6ff691334708b920
doppeltgaenger <stanislav.danylov@gmail.com>
2026-04-02 04:31:25 +0300

    chore(mock): add mock data for development

---
+++ src/data/users/users.js
@@ -0,0 +1,100 @@
+import avatarShei from "./assets/avatar-shei.webp";
+import avatarEnterpr1se from "./assets/avatar-enterpr1se.webp";
+import avatarSamara from "./assets/avatar-samara.webp";
+import avatarJani from "./assets/avatar-jani.webp";
+import avatarRoman from "./assets/avatar-roman.webp";
+import avatarStrongStyle from "./assets/avatar-strong-style.webp";
+import avatarHeelerCattle86 from "./assets/avatar-heeler-cattle86.webp";
+
+export const users = [
+  {
+    id: "ruizafon",
+    username: "RuiZafon",
+    avatar: "",
+    stats: {
+      editsAllTime: 2062494,
+      editsThisWeek: 23894,
+    },
+  },
+  {
+    id: "enterpr1se",
+    username: "enterpr1se",
+    avatar: avatarEnterpr1se,
+    stats: {
+      editsAllTime: 2532014,
+      editsThisWeek: 21182,
+    },
+  },
+  {
+    id: "shei",
+    username: "Shei",
+    avatar: avatarShei,
+    stats: {
+      editsAllTime: 2619505,
+      editsThisWeek: 14842,
+    },
+  },
+  {
+    id: "samara",
+    username: "Samara",
+    avatar: avatarSamara,
+    stats: {
+      editsAllTime: 4512422,
+      editsThisWeek: 13943,
+    },
+  },
+  {
+    id: "qualitylover",
+    username: "qualitylover",
+    avatar: "",
+    stats: {
+      editsAllTime: 1130677,
+      editsThisWeek: 13287,
+    },
+  },
+  {
+    id: "roman",
+    username: "Roman",
+    avatar: avatarRoman,
+    stats: {
+      editsAllTime: 538253,
+      editsThisWeek: 12156,
+    },
+  },
+  {
+    id: "strong-style",
+    username: "strong_style",
+    avatar: avatarStrongStyle,
+    stats: {
+      editsAllTime: 152460,
+      editsThisWeek: 7941,
+    },
+  },
+  {
+    id: "jani",
+    username: "Jani",
+    avatar: avatarJani,
+    stats: {
+      editsAllTime: 19941,
+      editsThisWeek: 6845,
+    },
+  },
+  {
+    id: "heeler-cattle86",
+    username: "HeelerCattle86",
+    avatar: avatarHeelerCattle86,
+    stats: {
+      editsAllTime: 382462,
+      editsThisWeek: 6438,
+    },
+  },
+  {
+    id: "bayramova",
+    username: "bayramova",
+    avatar: "",
+    stats: {
+      editsAllTime: 445300,
+      editsThisWeek: 6369,
+    },
+  },
+];
