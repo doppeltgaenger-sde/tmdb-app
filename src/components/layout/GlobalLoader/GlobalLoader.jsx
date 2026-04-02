f1e75d4173ebc6281b0fa94577c727024276ab05
doppeltgaenger <stanislav.danylov@gmail.com>
2026-04-02 04:26:52 +0300

    feat(layout): add app layout components

---
+++ src/components/layout/GlobalLoader/GlobalLoader.jsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import "./styles/GlobalLoader.scss";
+
+export const GlobalLoader = () => {
+  const loadingCount = useSelector((state) => state.app.loadingCount);
+  const [progress, setProgress] = useState(0);
+  const [visible, setVisible] = useState(false);
+
+  useEffect(() => {
+    let timer;
+
+    if (loadingCount > 0) {
+      setVisible(true);
+      setProgress(5);
+
+      timer = setInterval(() => {
+        setProgress((prev) => {
+          if (prev >= 85) return prev;
+          return prev + Math.random() * 8;
+        });
+      }, 800);
+    } else {
+      setProgress(100);
+
+      setTimeout(() => {
+        setVisible(false);
+        setProgress(0);
+      }, 900);
+    }
+
+    return () => clearInterval(timer);
+  }, [loadingCount]);
+
+  if (!visible) return null;
+
+  return <div className="global-loader" style={{ width: `${progress}%` }} />;
+};
