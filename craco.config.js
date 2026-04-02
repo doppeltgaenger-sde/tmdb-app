b9675ddbd35d955df7f25fde993d0523b1a69675
doppeltgaenger <stanislav.danylov@gmail.com>
2026-04-02 04:15:05 +0300

    chore: initialize app with base configuration

---
+++ craco.config.js
@@ -0,0 +1,22 @@
+const path = require("path");
+
+module.exports = {
+  webpack: {
+    alias: {
+      "@data": path.resolve(__dirname, "src/data"),
+      "@hooks": path.resolve(__dirname, "src/hooks"),
+      "@utils": path.resolve(__dirname, "src/utils"),
+      "@api": path.resolve(__dirname, "src/api"),
+      "@actions": path.resolve(__dirname, "src/redux/actions"),
+      "@reducers": path.resolve(__dirname, "src/redux/reducers"),
+      "@store": path.resolve(__dirname, "src/redux/store"),
+      "@thunk": path.resolve(__dirname, "src/thunk"),
+      "@shared": path.resolve(__dirname, "src/components/shared"),
+      "@layout": path.resolve(__dirname, "src/components/layout"),
+      "@base": path.resolve(__dirname, "src/components/base"),
+      "@blocks": path.resolve(__dirname, "src/components/blocks"),
+      "@pages": path.resolve(__dirname, "src/components/pages"),
+      "@styles": path.resolve(__dirname, "src/styles"),
+    },
+  },
+};
