6c9f42a5f2aa8b3410ef0a469e1f6c1adf4d249f
doppeltgaenger <stanislav.danylov@gmail.com>
2026-04-03 12:05:04 +0300

    fix(thunk): optimize media fetching logic

--- src/thunk/mediaThunk.js
+++ src/thunk/mediaThunk.js
@@ -8,17 +8,7 @@ import {
 import { normalizeMediaData } from "@utils";
 
 export const fetchMediaTrack = (track, tab, page = 1) => {
-  return async (dispatch, getState) => {
-    const state = getState();
-    const trackState = state.media.mediaTracks[track]?.[tab];
-
-    if (trackState?.data?.length && page === 1) {
-      return;
-    }
-
-    if (trackState?.loading) {
-      return;
-    }
+  return async (dispatch) => {
 
     try {
       dispatch(startGlobalLoading());
