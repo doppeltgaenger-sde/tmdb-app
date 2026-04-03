1205e80d07561d30426660b2ce9b9ab839f73645
doppeltgaenger <stanislav.danylov@gmail.com>
2026-04-03 10:15:57 +0300

    refactor(utils): improve color generation logic

--- src/utils/color/getUserColor.js
+++ src/utils/color/getUserColor.js
@@ -16,7 +16,7 @@ export const getUserColor = (key = "") => {
 
   for (let i = 0; i < key.length; i++) {
     hash = (hash << 5) - hash + key.charCodeAt(i);
-    hash |= 0; // приведение к 32-bit int
+    hash |= 0;
   }
 
   return COLORS[Math.abs(hash) % COLORS.length];
