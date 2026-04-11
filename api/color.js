a94f15ae5d7a279803bd057465bf437cecfb6e8f
doppeltgaenger <stanislav.danylov@gmail.com>
2026-04-12 00:23:22 +0300

    fix(chore): add updated api for media details hero

--- api/color.js
+++ api/color.js
@@ -1,43 +1,26 @@
-import sharp from "sharp";
+import jimp from "jimp";
 import axios from "axios";
 
 export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
+
   try {
     const { url } = req.query;
+    if (!url) return res.status(400).json({ error: "No url" });
 
-    if (!url) {
-      return res.status(400).json({ error: "No url" });
-    }
-
-    const response = await axios.get(url, {
-      responseType: "arraybuffer",
-    });
-
+    const response = await axios.get(url, { responseType: "arraybuffer" });
     const buffer = Buffer.from(response.data);
+    const image = await jimp.read(buffer);
+    
+    image.resize(1, 1);
+    
+    const hex = image.getPixelColor(0, 0);
+    const { r, g, b } = jimp.intToRGBA(hex);
 
-    const { data, info } = await sharp(buffer)
-      .resize(16, 16)
-      .raw()
-      .toBuffer({ resolveWithObject: true });
-
-    let r = 0, g = 0, b = 0;
-    const pixels = info.width * info.height;
-
-    for (let i = 0; i < data.length; i += info.channels) {
-      r += data[i];
-      g += data[i + 1];
-      b += data[i + 2];
-    }
-
-    res.status(200).json({
-      r: Math.round(r / pixels),
-      g: Math.round(g / pixels),
-      b: Math.round(b / pixels),
-    });
-
+    res.status(200).json({ r, g, b });
   } catch (e) {
-    console.error("COLOR API ERROR:", e);
-
-    res.status(500).json({ error: "color failed" });
+    console.error("API ERROR:", e.message);
+    res.status(500).json({ error: "Failed to process image" });
   }
 }
